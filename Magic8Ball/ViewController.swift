74c8d2a2fdb77d0e4a74cf440842982b78cd5136
Abel P <abelomar@icloud.com>
2018-09-06 16:41:16 -0700

    Initial Commit

---
+++ Magic8Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic8Ball
+//
+//  Created by zero on 9/6/18.
+//  Copyright © 2018 Abel C. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
