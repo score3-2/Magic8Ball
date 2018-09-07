f9981c0a79f53fe0fd84e9d977b16706647e6662
Abel P <abelomar@icloud.com>
2018-09-06 19:05:14 -0700

    Final Project
    Random Image per click. Also Shake Gesture Enabled.

--- Magic8Ball/ViewController.swift
+++ Magic8Ball/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Images Array
+    var images = [
+        "ball1",
+        "ball2",
+        "ball3",
+        "ball4",
+        "ball5",
+    ]
+    
+    // Random Property
+    var randomBall = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        askButton(self)
     }
-
+    
+    // Shake to change Image
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        askButton(self)
+    }
+    
+    // Conections
+    @IBOutlet weak var imageOfAnswers: UIImageView!
+    
+    @IBAction func askButton(_ sender: Any) {
+        randomBall = Int(arc4random_uniform(5))
+        imageOfAnswers.image = UIImage(named: images[randomBall])
+    }
+    
 
 }
 
